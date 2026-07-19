551d729eb8943cbab393d12ada47af38cc270596
daniel nyaks <dannynyamiaka@gmail.com>
2026-07-19 16:13:27 -0700

    set debug to false

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nc1)fbc)^i58ab9yly%-(tk698!kvbp@7($9vrj*i=sat#jx=9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
